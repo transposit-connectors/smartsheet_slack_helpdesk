9ea24c95dc3f6b0516ceee8b721d85fc736cfa0e
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-27 17:52:17 +0000

    testing

--- src/submit_ticket.js
+++ src/submit_ticket.js
@@ -1,14 +1,17 @@
 ({ http_event }) => {
+  var moment = require('moment.js');
   let body = JSON.parse(http_event.parsed_body.payload);
   let columns = api.run('this.list_columns', { sheetid: env.get('sheetid') });
-	return body;
+
   // Create the ticket entry in our databse
   api.run('this.add_rows', {
     						 sheetid: env.get('sheetid'),
                              user: api.run('this.get_email', {user: body.user.id})[0].email,
-                             title: body.submission.title,
+    						 date: moment().format("MM/DD/YYYY"),
                              description: body.submission.description,
-                             urgency: body.submission.urgency,
+                             priority: body.submission.priority,
+    						 team: body.submission.team,
+    						 department: body.submission.department,
     						 column2: columns[2].id,
     						 column3: columns[3].id,
     						 column4: columns[4].id,
@@ -16,25 +19,17 @@
     						 column6: columns[6].id,
     						 column7: columns[7].id,
     						 column11: columns[11].id,
+    						 column15: columns[15].id,
                            });
-  // Choose the description and return to user
-  if (body.submission.description) {
-    api.run('this.confirm_ticket', {
-      								channel: body.user.id,
-                                    header: `Ticket created for ${body.user.name}`,
-                                    title: body.submission.title,
-                                    description: body.submission.description,
-                                    urgency: body.submission.urgency,
-                                   });
-  } else {
-    api.run('this.confirm_ticket', {
-      								channel: body.user.id,
-                                    header: `Ticket created for ${body.user.name}`,
-                                    title: body.submission.title,
-                                    description: 'None provided',
-                                    urgency: body.submission.urgency,
-                                   });
-  }
+
+  // api.run('this.confirm_ticket', {
+  //     								channel: body.user.id,
+  //                                   header: `Ticket created for ${body.user.name}`,
+  //                                   description: body.submission.description,
+  //                                   priority: body.submission.priority,
+  //                                   team: body.submission.team,
+  //                                   department: body.submission.department,
+  //                                  });
     
   return { status_code: 200 };
 }
