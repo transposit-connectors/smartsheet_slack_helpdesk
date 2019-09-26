781bd17a135ef8a32ef6508c6aae3ebd7a18a938
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-26 17:43:27 +0000

    Commit from Transposit IDE

--- src/create_ticket.js
+++ src/create_ticket.js
@@ -1,5 +1,7 @@
 ({ http_event }) => {
-  let body = http_event.parsed_body;
-  api.run('this.open_dialog', {trigger_id: body.trigger_id, text: body.text});
+  setImmediate(() => {
+    let body = http_event.parsed_body;
+    api.run('this.open_dialog', {trigger_id: body.trigger_id, text: body.text});
+  });
   return { status_code: 200 };
 }
--- src/submit_ticket.js
+++ src/submit_ticket.js
@@ -1,36 +1,37 @@
 ({ http_event }) => {
-  var moment = require('moment.js');
-  let body = JSON.parse(http_event.parsed_body.payload);
-  let columns = api.query('SELECT id, title, type FROM smartsheet.list_columns WHERE sheetId=@sheetid', 
-      { sheetid: env.get('sheetid') });
-  // Create the ticket entry in our databse
-  api.run('this.add_rows', {
-    						 sheetid: env.get('sheetid'),
-    						 user: api.query('SELECT user.profile.email FROM slack.get_users_info WHERE user=@user', 
-                          		{user: body.user.id})[0].email,
-    						 date: new Date(moment().format("MM/DD/YYYY")),
-                             description: body.submission.description,
-                             priority: body.submission.priority,
-    						 team: body.submission.team,
-    						 department: body.submission.department,
-    						 column2: columns[2].id,
-    						 column3: columns[3].id,
-    						 column4: columns[4].id,
-    						 column5: columns[5].id,
-    						 column6: columns[6].id,
-    						 column7: columns[7].id,
-    						 column11: columns[11].id,
-    						 column15: columns[15].id,
-                           });
+  setImmediate(() => {
+    var moment = require('moment.js');
+    let body = JSON.parse(http_event.parsed_body.payload);
+    let columns = api.query('SELECT id, title, type FROM smartsheet.list_columns WHERE sheetId=@sheetid', 
+        { sheetid: env.get('sheetid') });
+    // Create the ticket entry in our databse
+    api.run('this.add_rows', {
+                               sheetid: env.get('sheetid'),
+                               user: api.query('SELECT user.profile.email FROM slack.get_users_info WHERE user=@user', 
+                                  {user: body.user.id})[0].email,
+                               date: new Date(moment().format("MM/DD/YYYY")),
+                               description: body.submission.description,
+                               priority: body.submission.priority,
+                               team: body.submission.team,
+                               department: body.submission.department,
+                               column2: columns[2].id,
+                               column3: columns[3].id,
+                               column4: columns[4].id,
+                               column5: columns[5].id,
+                               column6: columns[6].id,
+                               column7: columns[7].id,
+                               column11: columns[11].id,
+                               column15: columns[15].id,
+                             });
 
-  api.run('this.confirm_ticket', {
-      								channel: body.user.id,
-                                    header: `Ticket created for ${body.user.name}`,
-                                    description: body.submission.description,
-                                    priority: body.submission.priority,
-                                    team: body.submission.team,
-                                    department: body.submission.department,
-                                   });
-    
+    api.run('this.confirm_ticket', {
+                                      channel: body.user.id,
+                                      header: `Ticket created for ${body.user.name}`,
+                                      description: body.submission.description,
+                                      priority: body.submission.priority,
+                                      team: body.submission.team,
+                                      department: body.submission.department,
+                                     });
+  });
   return { status_code: 200 };
 }
